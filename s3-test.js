a1abfc5bb62a127a30326a1c46b47882f2c0cf26
Andrew Easterbrook <easterbrook@gmail.com>
2017-05-22 21:40:47 +1200

    .

--- s3-test.js
+++ s3-test.js
@@ -17,8 +17,9 @@ function dos3stuff() {
     },
     });
 
+    console.log(creds.accessKeyID);
     // take the converted file and copy it to s3
-    var params = {
+    /* var params = {
     localFile: 'samples/olsson.pdf',
     
     s3Params: {
@@ -39,7 +40,7 @@ function dos3stuff() {
     uploader.on('end', function() {
     console.log("done uploading");
     });
-
+    */
     }
 
 dos3stuff();
