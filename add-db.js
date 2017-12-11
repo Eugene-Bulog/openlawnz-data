226200f4e4b6a82820b6041921524c510ad43a3f
easterbrook@gmail.com <easterbrook@gmail.com>
2017-12-12 10:45:15 +1300

    .

--- add-db.js
+++ add-db.js
@@ -20,7 +20,7 @@ connection.connect(function(err){});
 // promise to read and parse JSON file, fulfilling once done
 var loadJSON = new Promise(
   function(fulfill, reject) {
-   fulfill(JSON.parse(fs.readFileSync('data-limited.json', 'utf8')));
+   fulfill(JSON.parse(fs.readFileSync('data-test-set.json', 'utf8')));
    reject(Error("Error loading JSON"));
   });
 
@@ -33,28 +33,49 @@ loadJSON.then(function(result) {
       _.map(data, function(cases) {
           
           // regex
-          var str = cases.CaseName;
-          // citation
-          const regCite = /(\[?\d{4}\]?) NZ(D|F|H|C|S|L)(A|C|R) (\d+)/;
-          var citation = str.match(regCite);
-          // casename
-          const regName = /^(.*?)\ \[/;
-          var caseName = str.match(regName);
-            
-          // fyi str.match() is array of all matches combined and then each individual group - [0] is all
-          // extracted case name is caseName[1] and extracted citation is citation[0] since will only be one citation match
-
-          // generate URL from cases.id
-          var url = 'https://forms.justice.govt.nz/search/Documents/pdf/' + cases.id;
-
-          // set downloaded to false
-          var downloaded = false;
+          if(cases.CaseName) {
           
-          // put case name, citation, url and downloaded status into database
-          connection.query('INSERT into caseinfo (case_name_full, citation, url, downloaded) VALUES (?, ?, ?, ?)', [caseName[1], citation[0], url, downloaded], function(err, rows, fields) {
-            if (!err) console.log("Inserted into database: " + caseName[1] + "\n"); 
-            else console.log("Error: " + err);
-          });  
+            var str = cases.CaseName;
+            // citation, with very basic regex error handling (should cope with unexpected whitespace)
+            const regCite = /(\[?\d{4}\]?)(\s*?)NZ(D|F|H|C|S|L)(A|C|R)(\s.*?)(\d+)*/;
+            var citation = str.match(regCite);
+            // casename - everything up to first square bracket
+            const regName = /^(.*?)\ \[/;
+            var caseName = str.match(regName);
+              
+            // fyi str.match() is array of all matches combined and then each individual group - [0] is all
+            // extracted case name is caseName[1] and extracted citation is citation[0] since will only be one citation match
+
+            // generate URL from cases.id
+            var url = 'https://forms.justice.govt.nz/search/Documents/pdf/' + cases.id;
+
+            // set downloaded to false
+            var downloaded = false;
+            
+            // put case name, citation, url and downloaded status into database
+            connection.query('INSERT into caseinfo (case_name_full, citation, url, downloaded, unique_id) VALUES (?, ?, ?, ?, ?)', [caseName[1], citation[0], url, downloaded, cases.id], function(err, rows, fields) {
+              if (!err) console.log("Inserted into database: " + caseName[1] + "\n"); 
+              else console.log("Error: " + err);
+              });  
+            }
+          else {
+           
+            var citation = "[0000] NZLR X"
+
+            var caseName = "Unknown";
+
+            // generate URL from cases.id
+            var url = 'https://forms.justice.govt.nz/search/Documents/pdf/' + cases.id;
+
+            // set downloaded to false
+            var downloaded = false;
+            
+            // put case name, citation, url and downloaded status into database
+            connection.query('INSERT into caseinfo (case_name_full, citation, url, downloaded, unique_id) VALUES (?, ?, ?, ?, ?)', [caseName, citation, url, downloaded, cases.id], function(err, rows, fields) {
+              if (!err) console.log("Unknown Case: " + url + "\n"); 
+              else console.log("Error: " + err);
+              })
+          }
           });
       });
   // close database 
---
+++ pdf2htmlinstallscript.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+# Ubuntu Developer Script For pdf2htmlEx
+# Created by Rajeev Kannav Sharma
+# http://rajeevkannav.github.io/
+#
+#
+# Downloads and configures the following:
+#
+#   CMake, pkg-config
+#   GNU Getopt
+#   GCC
+#   poppler
+#   fontforge
+#   pdf2htmlEX
+
+
+############################### How to use ###############################
+# [sudo] chmod 775 pdf2htmlEX.sh
+# [sudo] ./pdf2htmlEX.sh
+
+HOME_PATH=$(cd ~/ && pwd)
+LINUX_ARCH="$(lscpu | grep 'Architecture' | awk -F\: '{ print $2 }' | tr -d ' ')"
+POPPLER_SOURCE="http://poppler.freedesktop.org/poppler-0.33.0.tar.xz"
+FONTFORGE_SOURCE="https://github.com/coolwanglu/fontforge.git"
+PDF2HTMLEX_SOURCE="https://github.com/coolwanglu/pdf2htmlEX.git"
+
+if [ "$LINUX_ARCH" == "x86_64" ]; then
+ 
+  echo "Updating all Ubuntu software repository lists ..."
+apt-get update
+  echo "Installing basic dependencies ..."
+apt-get install -qq -y cmake gcc libgetopt++-dev git
+  echo "Installing Poppler ..."
+apt-get install -qq -y pkg-config libopenjpeg-dev libfontconfig1-dev libfontforge-dev poppler-data poppler-utils poppler-dbg
+
+  echo "Downloading poppler via source ..."
+wget "$POPPLER_SOURCE"
+tar -xvf poppler-0.33.0.tar.xz
+cd poppler-0.33.0/
+./configure --enable-xpdf-headers
+make
+make install
+
+  echo "Installing fontforge ..."
+cd "$HOME_PATH"
+apt-get install -qq -y packaging-dev pkg-config python-dev libpango1.0-dev libglib2.0-dev libxml2-dev giflib-dbg
+apt-get install -qq -y libjpeg-dev libtiff-dev uthash-dev libspiro-dev
+  echo "cloning fontforge via source ..."
+git clone --depth 1 "$FONTFORGE_SOURCE"
+cd fontforge/
+./bootstrap
+./configure
+make
+sudo make install
+
+  echo "Installing Pdf2htmlEx ..."
+cd "$HOME_PATH"
+git clone --depth 1 "$PDF2HTMLEX_SOURCE"
+cd pdf2htmlEX/
+cmake .
+make
+sudo make install
+
+echo 'export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
+source ~/.bashrc
+
+cd "$HOME_PATH" && rm -rf "poppler-0.33.0.tar.xz"
+cd "$HOME_PATH" && rm -rf "poppler-0.33.0"
+cd "$HOME_PATH" && rm -rf "fontforge"
+cd "$HOME_PATH" && rm -rf "pdf2htmlEX"
+
+else
+  echo "********************************************************************"
+  echo "This script currently doesn't supports $LINUX_ARCH Linux archtecture"
+fi
+ 
+echo "----------------------------------"
+echo "Restart your Ubuntu session for installation to complete..."
