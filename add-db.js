eb98e7e6d629113a3c2cbb04f83f8f0ab736402d
easterbrook@gmail.com <easterbrook@gmail.com>
2017-12-19 12:53:14 +1300

    Update json, add citation error handling, fix utf encoding

--- add-db.js
+++ add-db.js
@@ -7,16 +7,28 @@ var mysql = require('mysql');
 require('dotenv').config();
 
 // mysql connects
+
+// remember, charset should be utf8mb4 in db and connection
+// and edit my.cnf on any new mysql sever https://mathiasbynens.be/notes/mysql-utf8mb4
 var connection = mysql.createConnection({
   host  : process.env.DB_HOST,
   user  : process.env.DB_USER,
   password  : process.env.DB_PASS,
-  database  : 'caselaw'
+  database  : 'caselaw',
+  charset : 'UTF8MB4_UNICODE_CI'
 });
 
 connection.connect(function(err){});
 
 // promise to read and parse JSON file, fulfilling once done
+// nb to get updated json file (will need trimming) url is https://forms.justice.govt.nz/solr/jdo/select?q=*&facet=true&facet.field=Location&facet.field=Jurisdiction&facet.limit=-1&facet.mincount=1&rows=50000&json.nl=map&fq=JudgmentDate%3A%5B*%20TO%202017-12-19T23%3A59%3A59Z%5D&sort=JudgmentDate%20desc&fl=CaseName%2C%20JudgmentDate%2C%20DocumentName%2C%20id%2C%20score&wt=json&json.wrf=jQuery1123045201288903118564_1513632996819&_=1513632996820
+// that url gets up to 19 december 2017 - replace date variable
+// add    {"cases": [   at start
+// trim end so should end with    "score": 1.0 }]}
+// format at http://jsonviewer.stack.hu/
+// MOJ case ids and therefore urls subject to change retrospectively, links can break - get new JSON before any mass update
+
+
 var loadJSON = new Promise(
   function(fulfill, reject) {
    fulfill(JSON.parse(fs.readFileSync('data-test-set.json', 'utf8')));
@@ -38,27 +50,33 @@ loadJSON.then(function(result) {
             // citation, with very basic regex error handling (should cope with unexpected whitespace)
             const regCite = /(\[?\d{4}\]?)(\s*?)NZ(D|F|H|C|S|L)(A|C|R)(\s.*?)(\d+)*/;
             var citation = str.match(regCite);
-            // casename - everything up to first square bracket
-            const regName = /^(.*?)\ \[/;
-            var caseName = str.match(regName);
-              
-            // str.match() is array of all matches combined and then each individual group - [0] is all
-            // extracted case name is caseName[1] and extracted citation is citation[0] since will only be one citation match
-
-            // generate URL from cases.id
-            var url = 'https://forms.justice.govt.nz/search/Documents/pdf/' + cases.id;
-
-            // set downloaded to false for now
-            var downloaded = false;
             
-            // put case name, citation, url and downloaded status into database
-            connection.query('INSERT into caseinfo (case_name_full, citation, url, downloaded, unique_id) VALUES (?, ?, ?, ?, ?)', [caseName[1], citation[0], url, downloaded, cases.id], function(err, rows, fields) {
-              if (!err) console.log("Inserted into database: " + caseName[1] + "\n"); 
-              else console.log("Error: " + err);
-              });  
+            // if properly formatted citation exists, proceed
+            if (citation) {
+
+              // casename - everything up to first square bracket
+              const regName = /^(.*?)\ \[/;
+              var caseName = str.match(regName);
+                
+              // str.match() is array of all matches combined and then each individual group - [0] is all
+              // extracted case name is caseNameArray[1] and extracted citation is citation[0] since will only be one citation match
+              // generate URL from cases.id
+              var url = "https://forms.justice.govt.nz/search/Documents/pdf/" + cases.id;
+
+              // set downloaded to false for now
+              var downloaded = false;
+              
+              // put case name, citation, url and downloaded status into database
+              var insert = "INSERT into caseinfo (case_name_full, citation, url, downloaded, unique_id) VALUES (?, ?, ?, ?, ?)";
+              connection.query(insert, [caseName[1], citation[0], url, downloaded, cases.id], function(err, rows, fields) {
+                if (!err) console.log("Inserted into database: " + caseName[1] + "\n"); 
+                else console.log("Error: " + err);
+                });  
+              }
             }
+
           else {
-           
+          // either no casename, or no citation, so use defaults 
             var citation = "[0000] NZLR X";
 
             var caseName = "Unknown";
--- functions.js
+++ functions.js
@@ -6,12 +6,14 @@
 
 // VARIABLES, DEPENDENCIES ETC
 // ---------------------------
-var pdftohtml = require('pdftohtmljs');
-var AWS = require('aws-sdk');
-var s3 = require('s3');
-var async = require("async");
-var mysql = require('mysql');
-var limits = require('limits.js');
+const   pdftohtml = require('pdftohtmljs'),
+        AWS = require('aws-sdk'),
+        s3 = require('s3'),
+        async = require("async"),
+        mysql = require('mysql'),
+        limits = require('limits.js'),
+        download = require('download'),
+        fs = require('fs');
 
 // load creds
 require('dotenv').config();
@@ -20,7 +22,20 @@ require('dotenv').config();
 var creds = new AWS.SharedIniFileCredentials({profile: 'freelaw-s3'});
 AWS.config.credentials = creds; 
 
-/// get an array of pdf files from database
+// open s3 connection
+var client = s3.createClient({
+    maxAsyncS3: 20,     // this is the default 
+    s3RetryCount: 3,    // this is the default 
+    s3RetryDelay: 1000, // this is the default 
+    multipartUploadThreshold: 20971520, // this is the default (20 MB) 
+    multipartUploadSize: 15728640, // this is the default (15 MB) 
+    s3Options: {
+        accessKeyId: creds.accessKeyId,
+        secretAccessKey: creds.secretAccessKey,
+    },
+});
+
+/// open database
 var connection = mysql.createConnection({
     host  : process.env.DB_HOST,
     user  : process.env.DB_USER,
@@ -28,18 +43,63 @@ var connection = mysql.createConnection({
     database  : 'caselaw'
   });
 
-for(var caseid = 1; caseid < 5; caseid++) {
+// for testing - just do 1 cases
+for(var caseid = 16; caseid < 21; caseid++) {
 
-connection.query('SELECT case_name_full, url from caseinfo where caseid = ?', caseid, function(err, rows, fields) {
-    if (!err) {
-        console.log("Downloading: " + rows[0].case_name_full);
-         
-    }
-    else console.log("Error getting database info: " + err);
-    }); 
+var downloadCase = new Promise(function(resolve, reject) {
 
+    connection.query('SELECT case_name_full, url from caseinfo where caseid = ?', caseid, function(err, rows, fields) {
+        if (!err) {
+            console.log("Downloading: " + rows[0].case_name_full);
+            // download the case and save locally
+            download(rows[0].url).then(data => {
+                fs.writeFileSync(rows[0].case_name_full + '.pdf', data);
+
+                // upload to bucket
+                var params = {
+                    localFile: rows[0].case_name_full + '.pdf',
+                    s3Params: {
+                    Bucket: "nzhc-pdfs",
+                    Key: rows[0].case_name_full + ".pdf",
+                    },
+                };
+                var uploader = client.uploadFile(params);
+                uploader.on('error', function(err) {
+                    console.error("unable to upload:", err.stack);
+                });
+                uploader.on('progress', function() {
+                    console.log("Upload progress", (uploader.progressAmount / uploader.progressTotal) * 100);
+                });
+                uploader.on('end', function() {
+                    console.log("Done uploading" + rows[0].case_name_full);
+                    var filetoDelete = rows[0].case_name_full + '.pdf'; 
+                    // success for this file, so resolve promise
+                    resolve(filetoDelete);
+            });
+        });
+        }
+        else {
+            console.log("Error getting database info: " + err);
+            reject(err);
+        }
+        }); 
+    });
 }
 
+downloadCase
+    .then(function (resolve) {
+        // if promise resolved, file has downloaded locally, and uploaded to bucket, so delete
+        fs.unlink(resolve, function(error) {
+            if(error) { throw error; }
+            console.log("Deleted");
+        });
+    })
+    .catch(function(error) {
+        // promise rejected, log error
+        console.log(error);
+    });
+
+// close db connnection
 connection.end();
 
     /*
