ab1cd155d40de33b7277a8863481e0cda0e737c4
easterbrook@gmail.com <easterbrook@gmail.com>
2018-01-11 16:57:48 +1300

    generate temporary citations, add more regex

--- add-db.js
+++ add-db.js
@@ -2,6 +2,7 @@
 var fs = require("fs");
 var _=require("underscore");
 var mysql = require('mysql');
+var strings = require('./lib/string-functions');
 
 // load creds
 require('dotenv').config();
@@ -28,27 +29,30 @@ connection.connect(function(err){});
 // format at http://jsonviewer.stack.hu/
 // MOJ case ids and therefore urls subject to change retrospectively, links can break - get new JSON before any mass update
 
-// get today's date yyyy/mm/dd
-var today = new Date();
-var dd = today.getDate();
-var mm = today.getMonth()+1; //January is 0!
-var yyyy = today.getFullYear();
-if(dd<10){
-  dd='0'+dd;
-} 
-
-if(mm<10) {
-  mm='0'+mm;
-} 
-var today = yyyy+'/'+mm+'/'+dd;
 
 // get the JSON file
 var loadJSON = new Promise(
   function(fulfill, reject) {
-    fulfill(JSON.parse(fs.readFileSync('jsons/data-test-set.json', 'utf8')));
+    fulfill(JSON.parse(fs.readFileSync('jsons/count.json', 'utf8')));
     reject(Error("Error loading JSON"));
   });
-  
+ 
+  // get number of temporary citations in database already
+  // when dealing with both tables - need uniquely generated temporary/dummy citation, because it will be used for s3 bucket keys / urls
+  // generate the intended reference by looking up cases table as it holds master record of s3 keys and number of dummy citations
+  // in table caselaw.cases, generated_citation is boolean
+  // first get a count: look up last generated_citation row id and add one
+  var newGenID = 0;
+  var getLastDummy = "select count(*) as number from cases WHERE generated_citation = true";
+  connection.query(getLastDummy, function(error, results) {
+    if(error) throw error;
+        if(results[0]) {
+        // results[0].number is number of previously generated citations, so add one, if not leave as 0
+        var newGenID = results[0].number + 1;
+        console.log(newGenID);
+        }
+      });
+
   // parse the JSON
   loadJSON.then(function(result) {
     console.log("JSON Loaded. Processing Data.")
@@ -57,54 +61,63 @@ var loadJSON = new Promise(
   _.map(result, function(data) {
     _.map(data, function(cases) {
 
-      // if json casename data exists (some moj long names ie case + citation = "")
+      // if json casename data exists (some moj long names ie case + citation = "") and dealt with below
       if(cases.CaseName) {
         
-      var str = cases.CaseName;
-      // citation, with very basic regex error handling (should cope with unexpected whitespace)
-      const regCite = /(\[?\d{4}\]?)(\s*?)NZ(D|F|H|C|S|L)(A|C|R)(\s.*?)(\d+)*/;
-      var citation = str.match(regCite);
+        var str = cases.CaseName;
+        // citation, with very basic regex error handling (should cope with unexpected whitespace)
+        const regCite = /(\[?\d{4}\]?)(\s*?)NZ(D|F|H|C|S|L)(A|C|R)(\s.*?)(\d+)*/;
+        var citation = str.match(regCite);
 
-      // get judgment date - first 10 chars of cases.JudgmentDate should be yyyy/mm/dd
-      var fullDate = cases.JudgmentDate;
-      const regDate = /.{10}/;
-      var JudgmentDate = fullDate.match(regDate);
-      
-      // if properly formatted citation exists, proceed
-      if (citation) {
+        // get judgment date - first 10 chars of cases.JudgmentDate should be yyyy/mm/dd
+        var fullDate = cases.JudgmentDate;
+        const regDate = /.{10}/;
+        var JudgmentDate = fullDate.match(regDate);
         
-        // casename - everything up to first square bracket
-        const regName = /^(.*?)\ \[/;
-        var caseName = str.match(regName);
+        // if properly formatted citation exists, proceed
+        if (citation) {
           
-        // str.match() is array of all matches combined and then each individual group - [0] is all
-        // extracted case name is caseNameArray[1] and extracted citation is citation[0] since will only be one citation match
-        // generate URL from cases.id
-        var url = "https://forms.justice.govt.nz/search/Documents/pdf/" + cases.id;
-          
-        // set downloaded to false for now
-        var downloaded = false;
-          
-        // put case name, citation, url and downloaded status into database
-        var insertDownload = "INSERT into downloads (case_name_full, citation, url, downloaded, unique_id) VALUES (?, ?, ?, ?, ?)";
-        connection.query(insertDownload, [caseName[1], citation[0], url, downloaded, cases.id], function(err, rows, fields) {
-          if (!err) console.log("Inserted into downloads table: " + caseName[1] + "\n"); 
-          else console.log("Error inserting into downloads table: " + err);
-        });  
-        
-        var insertCases = "INSERT into cases (pdf_name, case_neutral_citation, case_date, case_name) VALUES (?, ?, ?, ?)";
-        connection.query(insertCases, [caseName[1] + '.pdf', citation[0], JudgmentDate[0], caseName[1]], function(err, rows, fields) {
-          if (!err) console.log("Inserted into cases table: " + caseName[1] + "\n"); 
-          else console.log("Error inserting into cases table: " + err);
-        });  
+          // casename - everything up to first square bracket
+          const regName = /^(.*?)\ \[/;
+          var caseName = str.match(regName);
+            
+          // str.match() is array of all matches combined and then each individual group - [0] is all
+          // extracted case name is caseNameArray[1] and extracted citation is citation[0] since will only be one citation match
+          // generate URL from cases.id
+          var url = "https://forms.justice.govt.nz/search/Documents/pdf/" + cases.id;
+            
+          // set downloaded to false for now
+          var downloaded = false;
+
+          // Can generate intended s3 bucket reference, which should be neutral citation (if found) without symbols
+          // ie 2012NZHC1256 or 2015NZSC38. If there is no citation available at time of initial import, will generate temporary one in 'else' below.
+          // Here we know citation exists therefore can proceed by trimming and inserting slash          
+          // remove spaces and brackets
+          const trim = /[^a-zA-Z0-9]/g;
+          var intendedBucket = citation[0].replace(trim, "");
+          intendedBucket = strings.insertSlash(intendedBucket, "/");
+
+          // mysql queries 
+          // put case name, citation, url and downloaded status into database
+          var insertDownload = "INSERT into downloads (case_name_full, citation, url, downloaded, moj_id, bucket_ref) VALUES (?, ?, ?, ?, ?, ?)";
+          connection.query(insertDownload, [caseName[1], citation[0], url, downloaded, cases.id, intendedBucket], function(err, rows, fields) {
+            if (!err) console.log("Inserted into downloads table: " + caseName[1] + "\n"); 
+            else console.log("Error inserting into downloads table: " + err + "\n");
+          });            
+
+          // fill in what we can into cases table, set generated citation = false since we have citation data
+          var insertCases = "INSERT into cases (pdf_name, bucket_ref, case_neutral_citation, case_date, case_name, generated_citation) VALUES (?, ?, ?, ?, ?, ?)";
+          connection.query(insertCases, [caseName[1] + '.pdf', intendedBucket, citation[0], JudgmentDate[0], caseName[1], false], function(err, rows, fields) {
+            if (!err) console.log("Inserted into cases table: " + caseName[1] + "\n"); 
+            else console.log("Error inserting into cases table: " + err + "\n");
+          });  
+          }
         }
-      }
           
       else {
-        // either no casename, or no citation, so use defaults 
+        // either there is no casename, or no citation, so use defaults 
         // still should add to download table for later download
-        // will have date, and MOJ unique id (eg /jdo/etc) which may be useful in cases table
-
+        newGenID++;
         var citation = "Unknown";
         var caseName = "Unknown";
         
@@ -115,29 +128,40 @@ var loadJSON = new Promise(
         var fullDate = cases.JudgmentDate;
         const regDate = /.{10}/;
         var JudgmentDate = fullDate.match(regDate);
-        
+        // get year - first 4 chars of cases.JudgmentDate should be yyyy/mm/dd, if no data then use 0000
+        // this is used to generate temporary citation data for cases table (irrelevant for downloads table which will just use 'unknown')
+        const regYear = /.{4}/;
+        var JudgmentYear = "0000";
+        if(JudgmentDate) {
+          var n = JudgmentDate.toString();
+          var JudgmentYear = n.match(regYear);
+        } 
+
         // set downloaded to false
         var downloaded = false;
+        // parse and generate dummy citation year plus /NZOL plus next number in sequence of dummy citations
+        var intendedBucket = JudgmentYear + "/NZOL" + newGenID;
         
-        // put case name, citation, url and downloaded status into temporary download database
-        var insertDownload = "INSERT into downloads (case_name_full, citation, url, downloaded, unique_id) VALUES (?, ?, ?, ?, ?)";
-        connection.query(insertDownload, [caseName, citation, url, downloaded, cases.id], function(err, rows, fields) {
-          if (!err) console.log("Unknown case inserted into downloads table: " + cases.id); 
-          else console.log("Error inserting unknown case into downloads table: " + err);
+        //mysql queries
+        // put case name, citation, url, intended bucket ref, and downloaded status into download table
+        var insertDownload = "INSERT into downloads (case_name_full, citation, url, downloaded, moj_id, bucket_ref) VALUES (?, ?, ?, ?, ?, ?)";
+        connection.query(insertDownload, [caseName, citation, url, downloaded, cases.id, intendedBucket], function(err, rows, fields) {
+          if (!err) console.log("Unknown case inserted into downloads table: " + intendedBucket); 
+          else console.log(err + "Error inserting unknown case into downloads table: " + intendedBucket);
         });
-        
-        // put the details we can extract from MOJ json (pdf name and judgment date)
-        var insertCases = "INSERT into cases (pdf_name, case_date) VALUES (?, ?)";
-        connection.query(insertCases, [cases.id, JudgmentDate[0]], function(err, rows, fields) {
-          if (!err) console.log("Inserted unknown case into cases table: " + cases.id); 
-          else console.log("Error inserting into cases table: " + err);
-        });
-      }
+        // put the details we can extract from MOJ json (pdf name and judgment date) plus bucket ref (generated temporary citation), and mark as generated citation
+        var insertCases = "INSERT into cases (pdf_name, case_date, bucket_ref, generated_citation) VALUES (?, ?, ?, ?)";
+        connection.query(insertCases, ['Unknown case', JudgmentDate, intendedBucket, true], function(err, rows, fields) {
+          if (!err) console.log("Inserted unknown case into cases table: " + intendedBucket); 
+          else console.log(err + "Error inserting unknown case into cases table: " + intendedBucket);
+          });
+        }
+
     });
   });
 
   // close database 
-  connection.end();
+  // connection.end();
   }, function(err) {
       console.log(err);
   });
--- index.js
+++ index.js
@@ -21,13 +21,28 @@ var delay = 1000;
 require('dotenv').config();
 
 // get aws creds - set profile, using profile set from ~/.aws/credentials
-var creds = new AWS.SharedIniFileCredentials({profile: 'node-s3'});
+var creds = new AWS.SharedIniFileCredentials({profile: 'node-s3-laptop'});
 AWS.config.credentials = creds; 
 
 // open s3 connection
 var s3 = new AWS.S3({
   params: {Bucket: 'freelaw-pdfs'}
 });
+
+// get today's date yyyy/mm/dd
+
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0
+var yyyy = today.getFullYear();
+if(dd<10){
+  dd='0'+dd;
+} 
+
+if(mm<10) {
+  mm='0'+mm;
+} 
+var today = yyyy+'/'+mm+'/'+dd; 
 
 // open database
 var connection = mysql.createConnection({
@@ -58,7 +73,7 @@ function doDownload (row) {
     // upload to bucket
       s3.upload(
           {
-            Key: row.case_name_full + '.pdf', 
+            Key: row.bucket_ref + '.pdf', 
             Body: fs.readFileSync(row.case_name_full + '.pdf')
           }, 
         function(err, data) {
@@ -71,12 +86,15 @@ function doDownload (row) {
     })
     
   .then(data => {
+      // delete file
       var filetoDelete = row.case_name_full + '.pdf'; 
       fs.unlink(filetoDelete, function(error) {
         if(error) { throw error; }
-      // set downloaded to true
+      // set downloaded to true in downloaded table
       // problem - must leave mysql connection open
-      connection.query('UPDATE downloads SET downloaded = 1 WHERE unique_id = ?', row.unique_id);
+      connection.query('UPDATE downloads SET downloaded = 1 WHERE bucket_ref = ?', row.bucket_ref);
+      // set pdf fetch date in cases table to current date
+      connection.query('UPDATE cases SET pdf_fetch_date = ? WHERE bucket_ref = ?', ['2018/01/11', row.bucket_ref]);
       });
     });
   }
@@ -88,7 +106,7 @@ function lookupURLs () {
     const promises = [];
     let n = 0;
 
-    connection.query('SELECT case_name_full, url, unique_id from downloads')
+    connection.query('SELECT id, case_name_full, url, bucket_ref from downloads WHERE id < 10')
     .on('error', reject)
 
     .on('result', (row) => promises.push(
