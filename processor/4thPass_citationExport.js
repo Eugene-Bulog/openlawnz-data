47550485819960865fd7ba36dcd3fe337fe94d32
easterbrook@gmail.com <easterbrook@gmail.com>
2018-05-03 16:18:45 +1200

    fixes

--- processor/4thPass_citationExport.js
+++ processor/4thPass_citationExport.js
@@ -9,16 +9,22 @@
 // ---------------------------
 const mysql = require('mysql');
 const fs = require('fs');
+const AWS = require('aws-sdk');
+var s3 = new AWS.S3({
+	params: { Bucket: 'openlawnz-searchindex' }
+});
+var creds = new AWS.SharedIniFileCredentials({ profile: process.env.PROFILE });
+AWS.config.credentials = creds;
 require('dotenv').config();
-
+console.log( process.env.PROFILE);
 
 var connection = mysql.createConnection({
-  host      : process.env.DB_HOST,
-  user      : process.env.DB_USER,
-  password  : process.env.DB_PASS,
-  database  : 'cases',
-  charset   : 'UTF8MB4_UNICODE_CI',
-  multipleStatements: true
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: 'cases',
+    charset: 'UTF8MB4_UNICODE_CI',
+    multipleStatements: true
 });
 
 connection.connect();
@@ -26,16 +32,30 @@ connection.connect();
 var obj = [];
 
 // connection.query("select * from case_citations; select * from cases", function(error, results, fields) {
-    
-connection.query("select * from case_citations; select * from cases", function(error, results, fields) {
-    for (var i = 0; i<results[0].length; i++) {
+
+connection.query("select * from case_citations", function (error, results, fields) {
+    for (var i = 0; i < results[0].length; i++) {
         console.log(results[0][i]);
         obj.push(results[0][i])
-        }
+    }
     fs.writeFileSync('citationJson.json', JSON.stringify(obj));
-});	
+        s3.upload({
+            Key: '/indexes/index.json',
+            Body: JSON.stringify(obj)
+        }, function(err, data) {
+            if (err) {
+
+                console.log(err)
+   
+            } else {
+   
+                console.log("Successfully uploaded data");
+   
+            }
+        })
+});
 connection.end();
-	
+
 
 
 
